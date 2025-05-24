0596851026bb34951bd259f918525f5220126e05
888i88 <igor_palun@icloud.com>
2025-05-24 13:57:19 +0200

    add prep exercise week3

--- Hack-my-future/Module_2 JavaScript/Week1/Practice exercises/test.js
+++
@@ -1,7 +0,0 @@
-let fancyNumber = 42; // Global Scope
-function fishy (numberFromOutside) {
-  return numberFromOutside; // ? 42
-  numberFromOutside = 33; // ? 33
-}
-fishy (fancyNumber);
-console.log(fancyNumber); // 42
--- Hack-my-future/Module_2 JavaScript/Week3/prep exercize/1-find-mentors.js
+++ Hack-my-future/Module_2 JavaScript/Week3/prep exercize/1-find-mentors.js
@@ -8,10 +8,12 @@ import { modules, students, mentors, classes } from "./hyf.js";
  *  ['John', 'Mary']
  */
 const possibleMentorsForModule = (moduleName) => {
-  // TODO complete this function
+  return mentors
+    .filter((mentor) => mentor.canTeach.includes(moduleName))
+    .map((mentor) => mentor.name);
 };
-// You can uncomment out this line to try your function
-// console.log(possibleMentorsForModule('using-apis'));
+
+console.log(possibleMentorsForModule("using-apis"));
 
 /**
  * Tjebbe wants to make it even easier for himself.
@@ -20,7 +22,13 @@ const possibleMentorsForModule = (moduleName) => {
  * It should return a single name.
  */
 const findMentorForModule = (moduleName) => {
-  // TODO complete this function
+  const possibleMentors = mentors.filter((mentor) =>
+    mentor.canTeach.includes(moduleName)
+  );
+  const randomIndex = Math.floor(Math.random() * possibleMentors.length);
+  if (possibleMentors.length === 0) {
+    return null;
+  }
+  return possibleMentors[randomIndex].name;
 };
-// You can uncomment out this line to try your function
-// console.log(findMentorForModule('javascript'));
+console.log(findMentorForModule("javascript"));
